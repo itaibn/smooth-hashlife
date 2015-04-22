58c84774c25827f0567a492b88f79c107e699c5a
Itai Bar-Natan <itaibn3141@gmail.com>
2015-04-21 22:18:36 -0400

    more to Model.hs

--- Model.hs
+++ Model.hs
@@ -4,13 +4,14 @@
 -- less optimized and simpler implementation
 module Model where
 
-import Prelude hiding (length, (!!))
-import Data.List (genericLength, genericIndex)
+import Prelude hiding (length, (!!), replicate)
+import Data.List (genericLength, genericIndex, genericReplicate)
 
 -- Integers for everything
 type I = Integer
 length = genericLength
 (!!) = genericIndex
+replicate = genericReplicate
 
 type Block = [[I]]
 
@@ -20,6 +21,11 @@ readBoard = map (map toInt) . lines
         toInt '.' = 1
         toInt '*' = 2
         toInt _ = error "Badly formed life position"
+
+showBlock = unlines . map (map fromInt)
+    where
+        fromInt 1 = '.'
+        fromInt 2 = '*'
 
 size :: Block -> I
 size = length
@@ -46,9 +52,9 @@ depthToSize n = leafsize * 2^n
 
 subblock :: I -> Block -> (I,I) -> Block
 subblock m b (i,j) = let
-    s = 2^m
-    n = i-2^(m-1)
-    w = j-2^(m-1)
+    s = 2^m*leafsize
+    n = i-2^m*(leafsize`div`2)
+    w = j-2^m*(leafsize`div`2)
   in
     --[b !! x !! y | x <- [n..n+s], y <- [w..w+s]]
     [[b !! y !! x | x <- [w..w+s-1]] | y <- [n..n+s-1]]
@@ -67,11 +73,7 @@ subblock m b (i,j) = let
 -- quadrant. focal i b returns the centers of the i-focal subblocks of the block
 -- b.
 focal :: I -> Block -> [(I, I)]
-{-
-focal n b = if not (checkSize (n+2) b) then error "Invalid board"
-    else if n==0 then
-        [b!!i!!j | i <- [], j <- []]
--}
+-- The defition currently in the C implementation:
 focal 0 b = let s = size b in
     [(i,j) | i <- [1..s-1], j <- [1..s-1]] -- ad hoc simplification of the interval
 focal i b = let
@@ -85,6 +87,31 @@ focal i b = let
     rating pt = hash (subblock i b pt)
     isFocal foc = all (<rating foc) (map rating (challengers foc))
     in filter isFocal posFiltered
+-- Perhaps a more elegant version:
+{-
+--focal 0 b = let s = size b in
+--    [(i,j) | i <- [1..s-1], j <- [1..s-1]] -- ad hoc simplification of the interval
+focal i b = let
+    siz = size b
+    base = if i > 0 then focal (i-1) b else [(i,j) | i <- [1..siz-1], j <-
+        [1..siz-1]]
+    posCond (n,m) = n >= depthToSize i && n < siz-depthToSize i && m >=
+        depthToSize i && m < siz-depthToSize i
+    posFiltered = filter posCond base
+    isNearby (n,m) (t,u) = max (abs (n-t)) (abs (m-u)) <= depthToSize i `div` 2
+    challengers foc = filter (isNearby foc) $ filter (/=foc) base
+    rating pt = hash (subblock i b pt)
+    isFocal foc = all (<rating foc) (map rating (challengers foc))
+    in filter isFocal posFiltered
+-}
+
+fatten :: Block -> Block
+fatten b = rjust (empty size) size (map (rjust 1 size) b)
+    where
+        rjust c n xs = xs ++ replicate (n-length xs) c
+        empty n = replicate n 1
+        size = 2 ^ (ceiling (logBase 2 (fromIntegral (baseSize b))))
+        baseSize b = max (length b) (maximum (map length b))
 
 {-
 main = interact ((++"\n").show.hash.map (map toInt).lines)
